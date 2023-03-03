e3bae561634c7abd6eec60c921afd8549320d7dc
Vikram Singh Rawat <vrawat@3clogic.com>
2023-03-03 12:50:41 +0530

    added commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const cookieParser = require('cookie-parser');
+const app = express()
+const port = 3000
+app.use(cookieParser());
+var cors = require('cors')
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+app.use(
+    cors({
+        origin: "http://localhost:4200",
+        credentials: true,
+    })
+);
+app.get('/api', (req, res) => {
+    res.cookie('user',"vikram",{sameSite:'none',secure:true});
+   // res.cookie('user',"vikram");
+    res.send('Hello World!')
+    return res;
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
