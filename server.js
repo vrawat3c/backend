c766713bb1cd5b2b0c5b154c8b65d64b1b690231
Vikram Singh Rawat <vrawat@3clogic.com>
2023-03-09 18:07:33 +0530

    commit-7

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var cors = require('cors')
 // app.use(express.static('public'));
 app.use(
     cors({
-        origin: "https://1d9a-134-238-18-189.in.ngrok.io",
+        origin: "https://33fc-134-238-18-189.in.ngrok.io",
         credentials: true,
     })
 );
